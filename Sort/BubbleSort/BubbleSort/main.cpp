218e3e48c844bdfc28bbeed1e27c0bec963fe42b
Gor55555 <gor.khachatryan.00000@gmail.com>
2024-10-20 16:20:55 +0400

    BubbleSort

---
+++ Sort/BubbleSort/BubbleSort/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <iomanip>
+
+template <typename T>
+T GetRandom();
+
+template <>
+int GetRandom<int>() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> dis(0, 100);
+    return dis(gen);
+}
+
+template <>
+double GetRandom<double>() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_real_distribution<> dis(1.0, 100.0);
+    return dis(gen);
+}
+
+template <typename T>
+void RandomNumbers(std::vector<T>& v)
+{
+    std::cout << "\n";
+    std::cout << std::setw(15) << std::left << "RandomNumbers: ";
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        v[i] = GetRandom<T>();
+        std::cout << v[i] << (i != v.size() - 1 ? ", " : "");
+    }
+    std::cout << "\n";
+}
+
+template <typename T>
+void BubbleSort(std::vector<T>& v)
+{
+    std::cout << std::setw(15) << std::left << "BubbleSort: ";
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        for (size_t j = 0; j < v.size() - 1; ++j) 
+        {
+            if (v[j] > v[j + 1])
+                std::swap(v[j], v[j + 1]);
+        }
+    }
+
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        std::cout << v[i] << (i != v.size() - 1 ? ", " : "");
+    }
+    std::cout << "\n";
+}
+
+
+int main()
+{
+    size_t size;
+    std::cout << "Size: ";
+    std::cin >> size;
+    std::vector<int> v(size);
+    RandomNumbers(v);
+    BubbleSort(v);
+}
+
